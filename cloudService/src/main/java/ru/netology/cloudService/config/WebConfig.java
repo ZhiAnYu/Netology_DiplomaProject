03c830874c4f5a8ab0da3a4cda9eed08bee83f64
ZhiAnYu <zhidkovaayu@gmail.com>
2026-08-23 22:21:01 +0300

    Change webconfig/ add localhost:8080

--- cloudService/src/main/java/ru/netology/cloudService/config/WebConfig.java
+++ cloudService/src/main/java/ru/netology/cloudService/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowCredentials(true)
-                .allowedOrigins("http://localhost:8081")
+                .allowedOrigins("http://localhost:8080","http://localhost:8081")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("auth-token");
